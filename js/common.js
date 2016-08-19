0ef3840c58684a1a24c36016159afc819b025aac
向家红22 <541353071@qq.com>
2016-08-19 10:28:05 +0800

    *

--- js/common.js
+++ js/common.js
@@ -2,19 +2,6 @@
 (function($){
 	$().ready(function(){
 		
-		////slider
-		//if($('#slider').length){
-		//	var sliderLength=$('#slider .mui-slider-item').length-2;
-		//	var sliderDotHtml='';
-		//	for(var i=0;i<sliderLength;i++){
-		//		sliderDotHtml+='<div class="mui-indicator"></div>';
-		//	}
-		//
-		//	$('.mui-slider-indicator').html(sliderDotHtml);
-		//	$('.mui-slider-indicator .mui-indicator').eq(0).addClass('mui-active');
-		//}
-		
-		
 		var winScreen=window.screen.width;
 	    if(winScreen>=640){
 	      winScreen=320;
